06c957a05ef765c1221532a85303b4a6a79132a7
SHIN <akiyachiumi@gmail.com>
2025-05-11 18:32:18 +0800

    customize doc item layout, add date

---
+++ src/theme/DocItem/Content/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import clsx from "clsx";
+import { ThemeClassNames } from "@docusaurus/theme-common";
+import { useDoc } from "@docusaurus/plugin-content-docs/client";
+import Heading from "@theme/Heading";
+import MDXContent from "@theme/MDXContent";
+import styles from "./styles.module.css";
+/**
+ Title can be declared inside md content or declared through
+ front matter and added manually. To make both cases consistent,
+ the added title is added under the same div.markdown block
+ See https://github.com/facebook/docusaurus/pull/4882#issuecomment-853021120
+
+ We render a "synthetic title" if:
+ - user doesn't ask to hide it with front matter
+ - the markdown content does not already contain a top-level h1 heading
+*/
+function useSyntheticTitle() {
+  const { metadata, frontMatter, contentTitle } = useDoc();
+  const shouldRender =
+    !frontMatter.hide_title && typeof contentTitle === "undefined";
+  if (!shouldRender) {
+    return null;
+  }
+  return metadata.title;
+}
+export default function DocItemContent({ children }) {
+  const syntheticTitle = useSyntheticTitle();
+  const { frontMatter } = useDoc();
+
+  return (
+    <div className={clsx(ThemeClassNames.docs.docMarkdown, "markdown")}>
+      {syntheticTitle && (
+        <header>
+          <Heading as="h1">{syntheticTitle}</Heading>
+        </header>
+      )}
+      <span className={styles.docItemDate}>
+        {frontMatter.date.toLocaleDateString()} 發布
+      </span>
+      <MDXContent>{children}</MDXContent>
+    </div>
+  );
+}
